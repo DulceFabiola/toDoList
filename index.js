673ccb086d332473a6c64c66b36d45d46b6a4afe
Dulce Fabiola Rodríguez Miranda <fabiolarodriguez@wolfsellers.com>
2022-03-07 12:13:50 -0600

    check

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task")
+    if(document.querySelector('#newtask input').value.length <= 0){
+        alert("Please Add a Task")
     }
 
     else{
         document.querySelector('#tasks').innerHTML += `
             <div class="task">
                 <span id="taskname">
+                <input type="checkbox" class="completed-tasks"/>
                     ${document.querySelector('#newtask input').value}
                 </span>
                 <button class="delete">
@@ -15,11 +16,12 @@ document.querySelector('#push').onclick = function(){
             </div>
         `;
 
-        var current_tasks = document.querySelectorAll(".delete");
+        let current_tasks = document.querySelectorAll(".delete");
         for(const i=0; i<current_tasks.length; i++){
             current_tasks[i].onclick = function(){
                 this.parentNode.remove();
             }
         }
+
     }
 }
